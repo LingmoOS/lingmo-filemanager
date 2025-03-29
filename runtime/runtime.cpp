90be26c46f03944ec2c9e15962c33dac3ac79694
Lingmo <115727099+lingmo-dream@users.noreply.github.com>
2025-03-29 23:45:00 +0800

    Update runtime.cpp

--- runtime/runtime.cpp
+++ runtime/runtime.cpp
@@ -9,12 +9,3 @@ RunTime::RunTime(QObject *parent)
 RunTime::~RunTime()
 {
 }
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication app(argc, argv);
-
-    RunTime runTime;
-    
-    return app.exec();
-}
