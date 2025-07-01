82100244a96d0e68912530706a5f16c0a4332d8b
Antonio Tenani <adtenani@gmail.com>
2025-07-01 15:35:20 +0200

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 96.9% (95 of 98 strings)
    
    Translation: Lingmo OS/lingmo-filemanager
    Translate-URL: https://hosted.weblate.org/projects/lingmoos/lingmo-filemanager/pt_BR/

--- translations/pt_BR.ts
+++ translations/pt_BR.ts
@@ -39,7 +39,7 @@
     <message>
         <location filename="../helper/datehelper.cpp" line="20"/>
         <source>%1 minutes ago</source>
-        <translation>1 minuto atrás</translation>
+        <translation>%1 minutos atrás</translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="24"/>
@@ -49,7 +49,7 @@
     <message>
         <location filename="../helper/datehelper.cpp" line="26"/>
         <source>%1 hours ago</source>
-        <translation>1 hora atrás</translation>
+        <translation>1 hora(s) atrás</translation>
     </message>
     <message>
         <location filename="../helper/datehelper.cpp" line="30"/>
@@ -59,7 +59,7 @@
     <message>
         <location filename="../helper/datehelper.cpp" line="32"/>
         <source>%1 days ago</source>
-        <translation>1 dia atrás</translation>
+        <translation>1 dia(s) atrás</translation>
     </message>
 </context>
 <context>
@@ -121,7 +121,7 @@
     <message>
         <location filename="../dialogs/filepropertiesdialog.cpp" line="243"/>
         <source>%1 files</source>
-        <translation>1 arquivo</translation>
+        <translation>1 arquivo(s)</translation>
     </message>
 </context>
 <context>
@@ -129,7 +129,7 @@
     <message>
         <location filename="../model/foldermodel.cpp" line="278"/>
         <source>%1 item</source>
-        <translation>1 item</translation>
+        <translation>1 item(s)</translation>
     </message>
     <message>
         <location filename="../model/foldermodel.cpp" line="278"/>
